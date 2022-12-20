748f9006ac7bcd1d437bcff3d7ab4dfafd34038b
Richard Hernandez <richard.hernandez@ukho.gov.uk>
2022-12-20 18:33:44 +0000

    Add project files.

---
+++ WebApplication2/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var envName = builder.Environment.EnvironmentName;
+
+var cstring = builder.Configuration.GetConnectionString("Constr");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
